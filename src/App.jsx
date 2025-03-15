78ae15176176ff5b3c71adc4b51fa88b31a3c752
nethuchathuu <155764628+nethuchathuu@users.noreply.github.com>
2025-03-15 21:15:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import { CiMicrophoneOn } from "react-icons/ci";
+
+function App() {
+  return (
+    <div className="main">
+      <span>I am Talko, Your Advanced Virtual Assistant</span>
+      <button>Click here <CiMicrophoneOn /></button>
+    </div>
+  )
+}
+
+export default App
